6300e36f9b8945edd148b1e20b66d6269a6d6e6d
aishwaryapb <30960214+aishwaryapb@users.noreply.github.com>
2017-08-14 01:12:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,10 +88,6 @@ function createTemplate(data)
 }
 
 
-app.get('/:articleName', function(req,res){
-    var articleName=req.params.articleName;
-    res.send(createTemplate(articles[articleName]));
-});
 
 
 app.get('/', function (req, res) {
@@ -104,6 +100,12 @@ app.get('/counter', function (req,res){
     counter = counter + 1;
     res.send(counter.toString());
 });
+
+app.get('/:articleName', function(req,res){
+    var articleName=req.params.articleName;
+    res.send(createTemplate(articles[articleName]));
+});
+
 
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
