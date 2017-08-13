f866f92ffdcb5af58747cf0d368e23a2c1d3a0d7
aishwaryapb <30960214+aishwaryapb@users.noreply.github.com>
2017-08-13 21:36:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,20 +5,97 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articles = {
+    'article-one' : {
+        title:'Article One|Aishwarya PB',
+        heading:'Article One',
+        date:'Aug 13, 2017',
+        content:
+            `<p>
+                This is the content for my first article. This is the content for my first article. This is the content for my first article. 
+                This is the content for my first article. This is the content for my first article. This is the content for my first article. 
+            </p>
+            
+            <p>
+                This is the content for my first article. This is the content for my first article. This is the content for my first article. 
+                This is the content for my first article. This is the content for my first article. This is the content for my first article. 
+            </p>
+            
+            <p>
+                This is the content for my first article. This is the content for my first article. This is the content for my first article. 
+                This is the content for my first article. This is the content for my first article. This is the content for my first article. 
+            </p>`
+    },
+    'article-two' : {
+        title:'Article Two|Aishwarya PB',
+        heading:'Article Two',
+        date:'Aug 15, 2017',
+        content:
+            `<p>
+                This is my second Article.
+            </p>`
+    },
+    'article-three' :{
+        title:'Article Three|Aishwarya PB',
+        heading:'Article Three'
+        date:'Aug 23, 2017',
+        content:
+            `<p>
+                This is the content for my third article.
+            </p>`
+    }
+};
+
+
+function createTemplate(data)
+{
+    var title=data.title;
+    var heading=data.heading;
+    var date=data.date;
+    var content=data.content;
+    
+    var htmlTemplate = 
+        `
+        <html>
+        <head>
+            <title>${title} </title>
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+        
+            <body>
+                <div class="container">
+                    <div>
+                        <a href='/'>Home</a>
+                    </div>
+                    <hr/>
+                    <h3>
+                        ${heading}
+                    </h3>
+                    <div>
+                        ${date}
+                    </div>
+                    <div>
+                        ${content}
+                    </div>
+                </div>
+            </body>
+            
+        </html>
+        
+        `;
+    return htmlTemplate;
+}
+
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
-});
-
-app.get('/article-two', function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-
-app.get('/article-three', function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+app.get('/:articleName', function(req,res){
+    var articleName=req.params.articleName;
+    res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
