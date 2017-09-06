de09af6b7fab6299963719539db68c2f13987bc1
aishwaryapb <30960214+aishwaryapb@users.noreply.github.com>
2017-09-07 02:43:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config = {
+    user:'aishbannimatti',
+    database:'aishbannimatti',
+    db:'db.imad.hasura-app.io',
+    port:'5432',
+    password: process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -89,9 +98,21 @@ function createTemplate(data)
 
 
 
-
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var pool = new Pool(Config);
+app.get('/test-db', function (req, res) {
+    //make a select request and return a response
+    pool.query("SELECT * from article", function(err,result){
+        if(err) {
+            res.status(500).send(err.toString());
+        }
+        else {
+            res.send(JSON.stringify(result));
+        }
+    });
 });
 
 var counter =0;
