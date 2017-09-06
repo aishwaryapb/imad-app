17374e276d7cd147b72aa4ccf5c9260ea4e9a046
aishwaryapb <30960214+aishwaryapb@users.noreply.github.com>
2017-09-07 02:55:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var Pool = require('pg').Pool;
 var config = {
     user:'aishbannimatti',
     database:'aishbannimatti',
-    db:'db.imad.hasura-app.io',
+    host:'db.imad.hasura-app.io',
     port:'5432',
     password: process.env.DB_PASSWORD
 };
