1e408dec1f11fb5367363b715a1070524a8107ac
aishwaryapb <30960214+aishwaryapb@users.noreply.github.com>
2017-09-07 16:21:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -135,6 +135,11 @@ app.get('/check-login', function(req, res) {
    }
 });
 
+app.get('/logout', function(req,res) {
+   delete req.session.auth;
+   res.send('Logged out successfully');
+});
+
 var pool = new Pool(config);
 app.get('/test-db', function (req, res) {
     //make a select request and return a response
