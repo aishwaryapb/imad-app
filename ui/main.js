628dc7d3b9be18b0d0f5767ba633a92417dea58b
aishwaryapb <30960214+aishwaryapb@users.noreply.github.com>
2017-09-07 15:51:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,36 +1,8 @@
 console.log('Loaded!');
 
-//counter code
 
 
-var btn=document.getElementById("counter");
-btn.onclick = function() {
-    
-    //Create a request
-    var request = new XMLHttpRequest();
-    
-    //Capture the response and store it in a variable
-    request.onreadystatechange = function() {
-        if(request.readyState == XMLHttpRequest.DONE)
-        {   
-            //Take action
-            if(request.status == 200) {
-                var counter = request.responseText;
-                var span=document.getElementById('count');
-                span.innerHTML=' '+counter.toString()+' ';
-            }
-        }
-        
-        //Not done yet
-        
-    };
-
-    //Make request
-    request.open('GET','http://aishbannimatti.imad.hasura-app.io/counter',true);
-    request.send(null);
-};
-
-//Submit name
+//Submit username / password to login
 
 
 var submit = document.getElementById('submit_btn');
@@ -41,21 +13,16 @@ submit.onclick=function() {
     
     //Capture the response and store it in a variable
     request.onreadystatechange = function() {
-        if(request.readyState == XMLHttpRequest.DONE)
+        if(request.readyState === XMLHttpRequest.DONE)
         {   
             //Take action
-            if(request.status == 200) {
-                 //Capture the list of names and render the list
-                var names=request.responseText;
-                names=JSON.parse(names);
-                var list = '';
-                for(var i=0;i<names.length;i++)
-                {
-                    list += '<li>'+names[i]+'</li>';
-                }
-                
-                var ul=document.getElementById('namelist');
-                ul.innerHTML=list;
+            if(request.status === 200) {
+                console.log('User logged in successfully');
+                alert('Logged in successfully');
+            } else if(request.status === 403) {
+                alert('Username/Password invalid');
+            } else if(request.status == 500) {
+                alert('Something went wrong on the server');
             }
         }
         
@@ -64,11 +31,11 @@ submit.onclick=function() {
     };
 
     //Make request
-    var nameInput=document.getElementById('name');
-    var name = nameInput.value;
-    
-    request.open('GET','http://aishbannimatti.imad.hasura-app.io/submit-name?name=' + name,true);
-    request.send(null);
+    var username=document.getElementById('username').value;
+    var password=document.getElementById('password').value;
+    request.open('POST','http://aishbannimatti.imad.hasura-app.io/login' + name,true);
+    request.setRequestHeader('Content-Type','application/json');
+    request.send(JSON.stringify({username: username, password: password}));
     
     //Capture the list of names and render the list
  
